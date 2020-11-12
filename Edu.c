bcf2aa637d82f257b01c857a809a4d08887654d7
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 16:49:07 +0000

    Learn

--- Edu.c
+++ Edu.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
 int main(void){
-    printf("d\na\nb\n!\n");
+    int height, length, width, volume;
+    float profit, loss;
+
+    height = 8;
+    length = 12;
+    width = 10;
+    volume = height * length * width;
+    profit = 10123.612f;
+
+    printf("Volume is: %d.\n", volume);
     return 0;
 }
