b262f3e596764b017764377fbc18456046f6f384
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 16:52:28 +0000

    learn

--- Edu.c
+++ Edu.c
@@ -2,14 +2,16 @@
 
 int main(void){
     int height, length, width, volume;
-    float profit, loss;
+    float profit, loss, net;
 
     height = 8;
     length = 12;
     width = 10;
     volume = height * length * width;
-    profit = 10123.612f;
-
-    printf("Volume is: %d.\n", volume);
+    profit = 10123.61f;
+    loss = 2131.51f;
+    net = profit - loss;
+    
+    printf("Volume is: %d.\nNet profit = %f", volume, net);
     return 0;
 }
