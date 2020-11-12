bc746549923526ff464bf11c187cb51f74eef7d8
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 20:00:34 +0000

    better

--- chapterOne.c
+++ chapterOne.c
@@ -5,9 +5,10 @@ int main(void){
 
     printf("        *\n       *\n      *\n*    *\n *  *\n   *"); // 1.
 
-    radius = 10.0f;
+    printf("\nEnter the radius of the sphere: ");
+    scanf("%f",&radius);
     volume = (4.0f/3.0f) * 3.14159265359 * (radius * radius * radius);
-    printf("\nThe volume of a sphere with a radius of 10 = %f\n",volume); //2
+    printf("The volume of a sphere with a radius of %.2f = %f\n",radius,volume); //2
 
     return 0;
 }
