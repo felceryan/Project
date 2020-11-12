019faecc08e47ed7a1d42052868f21118c43803e
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 18:24:59 +0000

    uh huh

--- Tempcalculator.c
+++ Tempcalculator.c
@@ -1,3 +1,18 @@
+#include <stdio.h>
+
+#define FREEZING_POINT 32.0f
+#define SCALE_FACTOR (5.0f / 9.0f)
+
 int main(void){
+    float celsius, fahrenheit;
+
+    printf("Enter temperature in fahrenheit: ");
+    scanf("%f", &fahrenheit);
+
+    celsius = (fahrenheit - FREEZING_POINT) * SCALE_FACTOR;
+    printf("Your temperature in celsius is: %.1f\n", celsius);
+    
+    
+
     return 0;
 }
