1c8287d4bb2f47f327c7def5ea01158099f95fd1
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 18:06:24 +0000

    Volume Calculate whatever

--- Edu.c
+++ Edu.c
@@ -1,16 +1,22 @@
 #include <stdio.h>
+#define INCHES_PER_POUND 166
 
 int main(void){
-    int weight, volume, height, width, length;
-    height = 12;
-    width = 16;
-    length = 16;
-    volume = height * width * length;
-    weight = (165 + volume) / 166;
+    
+    int height, width, length, volume, weight;
+    
+    printf("What is the height: ");
+    scanf("%d", &height);
+    printf("What is the width: ");
+    scanf("%d", &width);
+    printf("What is the length: ");
+    scanf("%d", &length);
 
-    printf("Dimensions = %d x %d x %d.\n",height, width, length);
-    printf("Volume (cubic inches) = %d.\n",volume);
-    printf("Dimensional weight (pounds) = %d.\n",weight);
+    volume = height * width * length;
+    printf("The volume is: %d\n",volume);
+
+    weight = ((INCHES_PER_POUND - 1) + volume) / INCHES_PER_POUND;
+    printf("Weight (pounds): %d", weight);
 
 
     return 0;
