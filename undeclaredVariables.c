eb54783b952e11a637f7e79c691475a7fd90796a
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-12 19:37:54 +0000

    ye[

---
+++ undeclaredVariables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void){
+    int x, y, z;
+    float a, b, c;
+    
+    printf("x = %d\ny = %d\nz = %d\n",x, y, z);
+    printf("a = %f\nb = %f\nc = %f\n",a, b, c);
+
+    return 0;
+}
