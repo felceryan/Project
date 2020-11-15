9f2035ae49560533d9bcfd5d7a51cdbced333cde
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-15 22:15:41 +0000

    learning

---
+++ C++/Chapter2/exercises2.2.3.cpp
@@ -0,0 +1,15 @@
+// int double = 3.14; 
+// Invalid it is not allowed to be called double as that is reserved
+
+// int _;
+// Valid
+
+// int catch-22;
+// Invalid, it doesn't allowed -, only _ and uppercase and lowercase letters and numbers
+
+// int 1_or_2 = 1;
+// Invalid, it's not allowed to begin with a number
+
+// double Double = 3.14;
+// Valid
+
