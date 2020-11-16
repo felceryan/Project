f680a335a71b696c0ed45204655d627c91539688
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-16 02:31:15 +0000

    const

---
+++ C++/Chapter2/exercises2.4.cpp
@@ -0,0 +1,8 @@
+// const int buf;
+// Not legal as const variables must be defined.
+// int cnt = 0;
+// legal
+// const int sz = cnt;
+// legal
+// ++cnt; ++sz;
+// ++cnt is legal however ++sz is illegal as const variables cannot be changed.
