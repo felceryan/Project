7730db0c107b315c2f668df62643a2aac92ca7fa
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-16 01:41:08 +0000

    ez

---
+++ C++/Chapter2/exercises2.3.3.cpp
@@ -0,0 +1,10 @@
+// int* ip, &r = ip;
+//  ip is a pointer to int, r is invalid.
+// *&r = ip; would fix it making it a reference to pointer ip.
+
+// int i, *ip = 0;
+// i is an int, ip is a pointer that points to null.
+
+// int* ip, ip2;
+// ip is a pointer to int, ip2 is an int.
+
