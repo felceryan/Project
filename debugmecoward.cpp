9ba7ed42772c0753a3f909a3a7e96ff0b92a7ff7
felceryan <38838148+felceryan@users.noreply.github.com>
2020-11-17 00:58:00 +0000

    melvin

--- debugmecoward.cpp
+++ debugmecoward.cpp
@@ -9,7 +9,10 @@ int main(){
     int loop;
     stinky _player;
     _player.damage = 30; _player.health = 24.55;
-    while(std::cin >> loop){}
+    while(1){
+        std::cin >> loop;
+        std::cout << _player.health << std::endl;
+    }
 
     return 0;
 }
